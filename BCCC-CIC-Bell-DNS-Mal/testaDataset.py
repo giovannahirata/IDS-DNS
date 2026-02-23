df28d7ebe83ae45e4b576519998aff3c12a6d973
Giovanna Luisa Hirata dos Anjos <giovannaluisahirata@gmail.com>
2026-02-23 19:34:01 -0300

    remoção da feature 'src_port' dos treinamentos dos modelos SVM, KNN e MLP, para classificação binária e multiclasse de cada um, respectivamente

--- BCCC-CIC-Bell-DNS-Mal/testaDataset.py
+++ BCCC-CIC-Bell-DNS-Mal/testaDataset.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('output-of-benign-pcap-3.csv')  # ou use encoding='utf-8' / 'latin1' se der erro
+df = pd.read_csv('output-of-benign-pcap-3.csv')
+df_br = pd.read_csv('datasets-br/output-of-spam-br-pcap.csv') 
+df_inter = pd.read_csv('datasets-inter/output-of-spam-inter-pcap.csv')
 
 # print(df.head())             # Ver as primeiras linhas
 # print(df.columns)           # Ver nomes das colunas
@@ -34,9 +36,9 @@ testar pegar 70% de cada categoria ao invés de 70% de todo o dataset com
 as categorias concatenadas
 """
 
-# filtro para criar dataset com dominios brasileiros (.br)
-pattern = r"\.br$"
-mask = df["dns_domain_name"].str.contains(pattern, regex=True)
-df_br = df[mask]
-print(df_br)
-print(df_br["dns_domain_name"].nunique())
+# print(f"Número de domínios originais: {df["dns_domain_name"].nunique()}")
+# print(f"Número de domínios brasileiros: {df_br["dns_domain_name"].nunique()}")
+# print(f"Número de domínios internacionais (sem .br): {df_inter["dns_domain_name"].nunique()}")
+# print(f"Soma dos domínios brasileiros e internacionais: {(df_br["dns_domain_name"].nunique())+(df_inter["dns_domain_name"].nunique())}")
+
+print(df["dst_port"].unique())
